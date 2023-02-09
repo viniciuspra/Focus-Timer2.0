5d58b66f66c91a94a36c2a5f5b21e3beb206fa6c
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-02-09 15:45:27 -0300

    update focus timer 2.0

--- js/index.js
+++ js/index.js
@@ -1,27 +1,76 @@
-import Timer from './timer.js'
-import {
-  minutesDisplay,
-  secondsDisplay,
-  playButton,
-  stopButton,
-  moreFiveButton,
-  lessFiveButton,
-  fireplaceAudioButton,
-  forestAudioButton,
-  rainAudioButton,
-  coffeeAudioButton
-} from './elements.js'
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
+const playButton = document.querySelector(".play")
+const stopButton = document.querySelector(".stop")
+const addFiveButton = document.querySelector(".more")
+const removeFiveButton = document.querySelector(".less")
+const forestAudioButton = document.querySelector(".forest")
+const rainAudioButton = document.querySelector(".rain")
+const coffeeAudioButton = document.querySelector(".coffee-shop")
+const fireplaceAudioButton = document.querySelector(".fireplace")
+let minutes = Number(minutesDisplay.textContent)
+let seconds = Number(secondsDisplay.textContent)
+let TimerTimeOut
+let check = false
 
-const timer = Timer({ 
-    minutesDisplay, 
-    secondsDisplay 
-  })
+function updateDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+function countdown() {
+  TimerTimeOut = setTimeout(() => { 
+      let minutes = Number(minutesDisplay.textContent)
+      let seconds = Number(secondsDisplay.textContent)
 
-playButton.addEventListener('click', () => {
-  timer.countdown();
+      updateDisplay(minutes, 0)
+
+      if (minutes <= 0 && seconds <= 0) {
+        updateDisplay(0, 0)
+        return
+      }
+
+      if (seconds <= 0) {
+        seconds = 60
+        --minutes
+      }
+      
+      updateDisplay(minutes, Number(seconds - 1))
+
+      countdown()
+  }, 1000)
+}
+
+function resetDisplay() {
+  updateDisplay(minutes, 0)
+  clearTimeout(TimerTimeOut)
+}
+
+
+playButton.addEventListener("click", () => {
+  if (!check) {
+    countdown()
+
+    check = true
+  }
 })
 
-stopButton.addEventListener('click', () => {
-  timer.reset();
-});
+stopButton.addEventListener("click", () => {
+  check = false
+  resetDisplay()
+})
 
+addFiveButton.addEventListener("click", () => {
+  if (minutes >= 95) {
+    return
+  }
+  minutes = minutes + 5
+  resetDisplay()
+  check = false
+})
+removeFiveButton.addEventListener("click", () => {
+  if (minutes == 0) {
+    return
+  }
+  minutes = minutes - 5
+  resetDisplay()
+})
